d9b7fa2642f79b77edc813554017c5f7e87c7764
JM <woalsdl500@naver.com>
2019-08-07 18:10:33 +0900

    Update base.py ADD kalman filter

--- base.py
+++ base.py
@@ -43,48 +43,126 @@ def region_of_interest(img, vertices):
 def weighted_img(img, initial_img, a=0.8, b=1., c=0.):
     return cv2.addWeighted(initial_img, a, img, b, c)
 
-def main():
-    rho = 2
-    theta = np.pi/180
-    threshold = 100
-    min_line_length = 100
-    max_line_gap = 100
+def draw_lines(img, line, color=[0, 0, 255], thickness=10): # 선 그리기
+    for x1,y1,x2,y2 in line:
+        cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+            
+def average_lane(lane_data):
+    x1s = []
+    y1s = []
+    x2s = []
+    y2s = []
+    for data in lane_data:
+        x1s.append(data[0][0])
+        y1s.append(data[0][1])
+        x2s.append(data[0][2])
+        y2s.append(data[0][3])
     
-    #img = cv2.imread('../test/test_img.jpg', cv2.IMREAD_COLOR)
+    if np.isnan(np.mean(x1s)):
+        x1 = np.nan_to_num(np.mean(x1s))
+    else:
+        x1 = np.mean(x1s)
+    if np.isnan(np.mean(x2s)):
+        x2 = np.nan_to_num(np.mean(x2s))
+    else:
+        x2 = np.mean(x2s)
+    if np.isnan(np.mean(y1s)):
+        y1 = np.nan_to_num(np.mean(y1s))
+    else:
+        y1 = np.mean(y1s)
+    if np.isnan(np.mean(y2s)):
+        y2 = np.nan_to_num(np.mean(y2s))
+    else:
+        y2 = np.mean(y2s)
+    
+    return int(x1), int(y1), int(x2), int(y2)
 
+def perspective_transform(img,src,dst):
+    """
+    Execute perspective transform
+    """
+    img_size = (img.shape[1], img.shape[0])
+    m = cv2.getPerspectiveTransform(src, dst)
+    m_inv = cv2.getPerspectiveTransform(dst, src)
+
+    warped = cv2.warpPerspective(img, m, img_size, flags=cv2.INTER_LINEAR)
+    unwarped = cv2.warpPerspective(warped, m_inv, (warped.shape[1], warped.shape[0]), flags=cv2.INTER_LINEAR)  # DEBUG
+
+    return warped, unwarped, m, m_inv
+
+
+
+def main():
     capture = cv2.VideoCapture("../test/outside_clockwise.avi")
 
+    img_w = 720 #img.shape[0]
+    img_h = 380 #img.shape[1]
+    
+    # perspective transform
+    src = np.float32([[img_w*0.2, img_h*0.5],[0, img_h],[img_w, img_h*0.5], [img_w,img_h]])
+    dst = np.float32([[0, img_h*0.5],[0, img_h],[img_w, img_h*0.5], [img_w,img_h]])
+    
+    # ROI
+    pts = np.array([[0, img_h], [img_w*0.2, img_h*0.6], [img_w*0.8, img_h*0.6], [img_w,img_h]])
+    
     while True:    
         ret, img = capture.read()
-        img_w = 720#img.shape[0]
-        img_h = 380#img.shape[1]
+        
         img = cv2.resize(img,(img_w,img_h))
 
         ori_img = img
 
+        # mask -> gray -> blur -> canny
         img = mask_white_yellow(img)
         img = grayscale(img)
         img = gaussian_blur(img, 5)
         img = canny(img,40,80)
-
-        yTopMask = img_h*0.55
-
-        vertices = np.array([[0, img_h], [0, img_h*0.75], [img_w, img_h*0.75], [img_w,img_h]], np.int32)
-
+           
+        # image perspective transform
+        warped, unwarped, m, m_inv = perspective_transform(ori_img,src,dst)
+        
+        # 관심영역 추출
+        vertices = np.array(pts, np.int32)
         img = region_of_interest(img, [vertices])
-
+        
+        # hough Line detection
         #houghLines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_length, maxLineGap=max_line_gap)
         hough = cv2.HoughLinesP(img, 2, np.pi / 180, 100, np.array([]), minLineLength = 100, maxLineGap = 50)
 
-        foundLinesImage = np.zeros((img_h, img_w, 3), dtype=np.uint8)
+        if hough is not None:
+            line_arr = np.squeeze(hough,axis=1)
 
-        for line in hough:
-            for x1,y1,x2,y2 in line:
-                cv2.line(foundLinesImage, (x1, y1), (x2, y2), [255, 0, 0], 7)
+            slope_degree = (np.arctan2(line_arr[:,1] - line_arr[:,3], line_arr[:,0] - line_arr[:,2]) * 180) / np.pi
 
-        origWithFoundLanes = weighted_img(foundLinesImage,ori_img)
+            # 수평 기울기 제한
+            line_arr = line_arr[np.abs(slope_degree)<160]
+            slope_degree = slope_degree[np.abs(slope_degree)<160]
+            # 수직 기울기 제한
+            line_arr = line_arr[np.abs(slope_degree)>95]
+            slope_degree = slope_degree[np.abs(slope_degree)>95]
+            # 필터링된 직선 버리기
+            L_lines, R_lines = line_arr[(slope_degree>0),:], line_arr[(slope_degree<0),:]
+            temp = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+            L_lines, R_lines = L_lines[:,None], R_lines[:,None]
+
+            foundLinesImage = np.zeros((img_h, img_w, 3), dtype=np.uint8)
+            #foundLinesImage = np.zeros((img_h, img_w), dtype=np.uint8)
+
+            if L_lines is not None:
+                L_line = np.array([average_lane(L_lines)])
+                draw_lines(foundLinesImage,L_line)
+            if R_lines is not None:
+                R_line = np.array([average_lane(R_lines)])
+                draw_lines(foundLinesImage,R_line)
+
+            origWithFoundLanes = weighted_img(foundLinesImage,ori_img)
+
+        else:
+            origWithFoundLanes = ori_img
 
         cv2.imshow('image',origWithFoundLanes)
+        cv2.imshow('warp',warped)
+        cv2.imshow('unwarp',unwarped)
 
         if cv2.waitKey(33) > 0: break
 
